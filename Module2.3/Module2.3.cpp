13560d02fe9a9b4755412c96fed91690bcee1ca7
Sharik2468 <nsharabanov@bk.ru>
2022-08-21 22:02:13 +0300

    Initial commit

---
+++ Module2.3/Module2.3.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+
+template<class T>
+class Iterator
+{
+public:
+	typedef T value_type;
+	typedef T* pointer;
+	typedef T& reference;
+	typedef std::forward_iterator_tag iterator_category;
+	typedef ptrdiff_t difference_type;
+
+	Iterator(T* curr) :current(curr)
+	{
+	}
+	Iterator& operator =(const Iterator& other)
+	{
+		if (this != &other)
+		{
+			current = other.current;
+		}
+		return *this;
+	}
+	Iterator& operator ++()
+	{
+		++current;
+		return *this;
+	}
+	Iterator operator ++(int i)
+	{
+		Iterator tmp(current);
+		++current;
+		return tmp;
+	}
+	T& operator *()
+	{
+		return *current;
+	}
+	T* operator ->()
+	{
+		return current;
+	}
+	bool operator ==(const Iterator& other)
+	{
+		return current == other.current;
+	}
+	bool operator !=(const Iterator& other)
+	{
+		return !(*this == other);
+	}
+	int GetValue() { return 1; }
+private:
+	T* current;
+};
+
+int main()
+{
+	std::vector<int> v = { 1, 2, 3, 4, 5 };
+
+	Iterator<std::vector<int>> iter(&v); // получаем итератор
+
+	++iter;             // перемещаемся вперед на один элемент
+	std::cout << iter.GetValue() << std::endl;
+
+
+	return 0;
+}
